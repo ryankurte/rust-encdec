b07dfcf20e67cb64325b9a559fa6b53247522299
ryan kurte <ryankurte@gmail.com>
2022-09-08 11:56:26 +1200

    pin dependency versions

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 //! This is intend to provide a straightforward method of serialising objects 
 //! similar to (but not explicitly compatible with) C's packed representation.
 
+#![no_std]
+
 pub use encdec_base::{Encode, Decode};
 
 pub use encdec_macros::{Encode, Decode};
